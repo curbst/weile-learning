ec43930198098cf42d878aeb30b7a745cf42742e
Jianbing Zheng <jzheng1@cs.sierracollege.edu>
2022-03-08 23:12:26 -0800

    first commit scipt

---
+++ testfun.sh
@@ -0,0 +1,15 @@
+aa="aaaaaa"
+function log_msg {
+  local aa="bbbbbbb"
+  echo "[`date '+ %F %T'` ]: $@"
+  echo "${aa}"
+}
+log_msg "fuck!"
+echo "${aa}"
+
+food[0]=0
+food[1]=1
+food[4]=4
+food[5]=''
+food[6]=6
+echo ${#food[@]}
